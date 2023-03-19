ca9737f859a065e698e7c32e182cabd113c25eac
khHJP <kh00phj@gmail.com>
2023-03-19 20:13:59 +0900

    feat(craigChat): add craigChat.jsp
    - craigChat.jsp 추가
    - ChatController(craigChat) 추가

--- src/main/java/com/sh/oee/chat/controller/ChatController.java
+++ src/main/java/com/sh/oee/chat/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.sh.oee.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sh.oee.chat.model.service.ChatService;
 
@@ -9,8 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequestMapping("/chat")
 public class ChatController {
 
 	@Autowired
 	private ChatService chatService;
+	
+	@GetMapping("/craigChat.do")
+	public void craigChat() {}
 }
