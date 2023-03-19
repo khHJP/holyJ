1fb67b4f34f8dde8ccc56ad37474742ad1e59fb1
yeji0401 <splendideyee@gmail.com>
2023-03-19 14:13:29 +0900

    Notice DTO 수정

--- src/main/java/com/sh/oee/admin/controller/AdminController.java
+++ src/main/java/com/sh/oee/admin/controller/AdminController.java
@@ -1,5 +1,17 @@
 package com.sh.oee.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.sh.oee.admin.model.service.AdminService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
 public class AdminController {
+	
+	@Autowired
+	public AdminService adminService;
 
 }
