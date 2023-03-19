6e6d842e9979648dcdb10231f75c5d5f0222faf3
Lehanaa <eowk3508@gmail.com>
2023-03-19 20:15:29 +0900

    MemberController : getmapping추가 feat(memberDetail): add memberDetail.jsp추가 feat(mypage): add mypage.jsp 추가 feat(myprofile): add myprofile.jsp 추가

--- src/main/java/com/sh/oee/member/controller/MemberController.java
+++ src/main/java/com/sh/oee/member/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.sh.oee.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sh.oee.member.model.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequestMapping("/member")
 @Controller
 public class MemberController {
 
@@ -19,4 +21,65 @@ public class MemberController {
 	public String temp() {
 		return "common/temp";
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//--------------- 하나 시작----------------------------------------
+	@GetMapping("/myPage.do")
+	public void myPage() {}
+	@GetMapping("/myProfile.do")
+	public void myProfile() {}
+	@GetMapping("/memberDetail.do")
+	public void memberDetail() {}	
+
+	// @RequestMapping("/member") 작성
+	// views에 member folder 생성후 myPage.jsp 생성
+	//-------------- 하나 끝 --------------------------------------------
 }
