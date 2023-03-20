b898202c3bd3289160adda4bcdbe48f50f52b94e
jungeun819 <xhxhxfh@gmail.com>
2023-03-20 15:03:19 +0900

    fetch(login) : add login

---
+++ src/main/java/com/sh/security/model/service/SecurityService.java
@@ -0,0 +1,15 @@
+package com.sh.security.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.sh.security.model.dao.SecurityDao;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class SecurityService {
+	
+	private SecurityDao securityDao;
+	
+}
