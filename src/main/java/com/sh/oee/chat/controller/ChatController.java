5030143de3a4db1529fef2ed1feb03b949425d73
khHJP <kh00phj@gmail.com>
2023-03-29 14:56:11 +0900

    feat(craigChatList): craigChatList finish
    - 판매자의 특정 게시물 관련 채팅목록 구현 성공
    - craigChatList.jsp 디자인
    - craigChatList.jsp 출력 성공

--- src/main/java/com/sh/oee/chat/controller/ChatController.java
+++ src/main/java/com/sh/oee/chat/controller/ChatController.java
@@ -41,12 +41,12 @@ public class ChatController {
 	private CraigService craigService;
 	@Autowired
 	private MemberService memberService;
-	
+
 	@GetMapping("/chatList.do")
 	public void chatList() {
-		
+
 	}
-	
+
 	/**
 	 * 중고거래 게시글 작성자가 본인 게시글에서 대화중인 채팅방 선택시
 	 */
@@ -54,7 +54,7 @@ public class ChatController {
 	public void craigChatList(@RequestParam int craigNo, Authentication authentication, Model model) {
 		// 1. 로그인한 사용자 id 꺼내기 (판매자)
 		String memberId = ((Member) authentication.getPrincipal()).getMemberId();
-		log.debug("판매자 = {}", memberId);
+//		log.debug("판매자 = {}", memberId);
 
 		// 2. memberId, craigNo로 chatroom_id 조회
 		Map<String, Object> craigChatMap = new HashMap<>();
@@ -63,41 +63,96 @@ public class ChatController {
 
 		// 3. 해당 chatroomId로 모든 채팅방 조회해 List에 담기
 		List<String> craigChatList = chatService.findCraigChatList(craigChatMap);
-		log.debug("채팅방id = {}", craigChatList);
-		
-		// 받은 메시지 list
-		List<CraigMsg> craigMsgs = new ArrayList<>(); 
-		
-		for(String chatroomId : craigChatList) {
-			CraigMsg lastChat = chatService.findLastCraigMsgByChatroomId(chatroomId);
-			if(lastChat != null) {
-				craigMsgs.add(lastChat);
+//		log.debug("채팅방id = {}", craigChatList);
+
+//		// 채팅방 List 순회하며 마지막채팅 꺼내기
+//		for(String chatroomId : craigChatList) {
+//			CraigMsg lastChat = chatService.findLastCraigMsgByChatroomId(chatroomId);
+//			if(lastChat != null) {
+//				Member writer = memberService.selectOneMember(lastChat.getWriter());
+//				chattingsMap.put("writer", writer);
+//				chattingsMap.put("lastChat", lastChat);
+//				chattings.add(chattingsMap);
+//			}
+//		}
+//		for(Map<String, Object> craigMap : chattings) {
+////		log.debug("craigMap = {}", craigMap);
+//		}
+//		
+		CraigMsg lastChat = new CraigMsg();
+
+//		log.debug("리스트보기 = {}", craigChatList);
+
+		// model로 전달할 대화상대 + 마지막메시지 객체
+		List<Map<String, Object>> chatModel = new ArrayList<>();
+
+		for (int i = 0; i < craigChatList.size(); i++) {
+
+			String chatroomId = craigChatList.get(i);
+			// 1. 채팅방id로 각 채팅방 마지막 메시지 가져옴
+			lastChat = chatService.findLastCraigMsgByChatroomId(chatroomId);
+
+			if (lastChat != null) {
+				// 2. 현재 사용자 id, 채팅방id로 각 채팅방의 대화상대 가져옴
+				Map<String, Object> startUser = new HashMap<>();
+				startUser.put("memberId", memberId);
+				startUser.put("chatroomId", chatroomId);
+				// 대화상대 id
+				String otherUserId = chatService.findOtherFromCraigChat(startUser);
+				// id로 Member객체 가져옴
+				Member otherUser = memberService.selectOneMember(otherUserId);
+
+				// 대화상대 닉네임
+				String otherName = otherUser.getNickname();
+				// 대화상대 동 정보
+				String otherDong = memberService.selectMydongName(otherUser.getDongNo());
+				// 대화상대 프로필이미지
+				String otherProf = otherUser.getProfileImg();
+
+				// 3. 대화상대 + 메시지 담을 map
+				Map<String, Object> chatMap = new HashMap<>();
+				chatMap.put("otherName", otherName);
+				chatMap.put("otherDong", otherDong);
+				chatMap.put("otherProf", otherProf);
+				chatMap.put("lastChat", lastChat);
+
+				// 4. map을 List에 추가
+				chatModel.add(chatMap);
 			}
 		}
-		log.debug("craigMsgs = {}", craigMsgs);
 
-		// 5. model에 대화중인 member객체를 담은 chatMembers 리스트 넣기
-		model.addAttribute("craigMsgs", craigMsgs);		
+		// 5. model에 List 넣어주기!
+		model.addAttribute("chatModel", chatModel);
+
+		// 6. model에 해당 중고거래 게시글 객체 담기
+		Craig craig = craigService.findCraigByCraigNo(craigNo);
+		model.addAttribute("craig", craig);
+
+		// 7. model에 해당 중고거래 게시글 이미지 담기
+		List<CraigAttachment> craigImg = craigService.selectcraigAttachments(craigNo);
+		model.addAttribute("craigImg", craigImg);
+
 	}
-	
+
 	/**
 	 * 중고거래 게시글에서 채팅하기 선택시
 	 */
 	@ResponseBody
 	@GetMapping("/craigChat/{craigNo}")
-	public Map<String, Object> craigChat(@PathVariable int craigNo, Authentication authentication, Model model, HttpSession session){
-		
+	public Map<String, Object> craigChat(@PathVariable int craigNo, Authentication authentication, Model model,
+			HttpSession session) {
+
 		// 1. 로그인한 사용자 id 꺼내기
 		String memberId = ((Member) authentication.getPrincipal()).getMemberId();
 		log.debug("구매자 = {}", memberId);
-		
+
 		// 2. 게시글 번호로 게시글객체 -> 판매자정보 꺼내기
-		// 게시글정보만 가져오는 메소드 새로 추가함! 
+		// 게시글정보만 가져오는 메소드 새로 추가함!
 		Craig craig = craigService.findCraigByCraigNo(craigNo);
-		
+
 		String sellerId = craig.getWriter();
 		log.debug("판매자 = {}", sellerId);
-		
+
 		// 3. memberId, craigNo로 chatroom_id 조회
 		// parameter는 1개만 가능하므로 Map에 담는다
 		Map<String, Object> craigChatMap = new HashMap<>();
@@ -105,25 +160,26 @@ public class ChatController {
 		craigChatMap.put("craigNo", craigNo);
 
 		String chatroomId = chatService.findCraigChatroomId(craigChatMap);
-	
-		 // 채팅방 첫 입장시 
-		if(chatroomId == null) { 
-		// 1. chatroomId 생성 chatroomId =
-		 chatroomId = generateCraigChatroomId(); 
-		 log.debug("채팅방번호 = {}", chatroomId);
-		  
-		 // 2. craig_chat 테이블에 2행 insert (로그인한 사용자memberId, 게시글 작성자 sellerId)
-		 List<CraigChat> chatMembers = Arrays.asList( new CraigChat(chatroomId,
-		 memberId, craigNo), new CraigChat(chatroomId, sellerId, craigNo)); int result
-		 = chatService.createCraigChatroom(chatMembers); 
-		 }
-		 
-		 Map<String, Object> map = new HashMap<>(); map.put("memberId", memberId);
-		 map.put("chatroomId", chatroomId);
+
+		// 채팅방 첫 입장시
+		if (chatroomId == null) {
+			// 1. chatroomId 생성 chatroomId =
+			chatroomId = generateCraigChatroomId();
+			log.debug("채팅방번호 = {}", chatroomId);
+
+			// 2. craig_chat 테이블에 2행 insert (로그인한 사용자memberId, 게시글 작성자 sellerId)
+			List<CraigChat> chatMembers = Arrays.asList(new CraigChat(chatroomId, memberId, craigNo),
+					new CraigChat(chatroomId, sellerId, craigNo));
+			int result = chatService.createCraigChatroom(chatMembers);
+		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("memberId", memberId);
+		map.put("chatroomId", chatroomId);
 
 		return map;
 	}
-	
+
 	/**
 	 * 중고거래 채팅방 chatroomId 생성
 	 */
@@ -132,15 +188,15 @@ public class ChatController {
 		Random random = new Random();
 		final int LEN = 12;
 		// 반복처리
-		for(int i = 0; i < LEN; i++) {
+		for (int i = 0; i < LEN; i++) {
 			// 숫자
-			if(random.nextBoolean()) {
+			if (random.nextBoolean()) {
 				chatroomId.append(random.nextInt(10));
 			}
 			// 영문자
 			else {
 				// 대문자
-				if(random.nextBoolean()) {
+				if (random.nextBoolean()) {
 					chatroomId.append((char) (random.nextInt(26) + 'A'));
 				}
 				// 소문자
@@ -151,20 +207,21 @@ public class ChatController {
 		}
 		return chatroomId.toString();
 	}
-	
+
 	/**
 	 * 중고거래 채팅방 팝업 열기
 	 */
 	@GetMapping("/craigChat.do")
-	public String craigChatPop(@RequestParam String chatroomId, @RequestParam String memberId, @RequestParam int craigNo, Model model) {
+	public String craigChatPop(@RequestParam String chatroomId, @RequestParam String memberId,
+			@RequestParam int craigNo, Model model) {
 		// 1. 채팅방을 연 사람이 누구인지 확인
 //		log.debug("연사람확인 = {}", memberId);
 		model.addAttribute("memberId", memberId);
-		
+
 		// 2. 전달받은 chatroomId로 대화내역 찾기
 		List<CraigMsg> craigMsgs = chatService.findCraigMsgBychatroomId(chatroomId);
 		model.addAttribute("craigMsgs", craigMsgs);
-		
+
 		// 3. 대화상대 정보 찾기
 		// 채팅방 아이디로 채팅방을 조회하고, 나온 멤버중 본인을 제외한 멤버 가져옴
 		Map<String, Object> startUser = new HashMap<>();
@@ -177,17 +234,17 @@ public class ChatController {
 //		log.debug("otherUser = {}", otherUser);
 		model.addAttribute("otherUser", otherUser);
 
-		// 4. 해당게시글 찾기 
+		// 4. 해당게시글 찾기
 		Craig craig = craigService.findCraigByCraigNo(craigNo);
 
 		// 5. 게시글 첨부파일 찾기
 		List<CraigAttachment> craigImg = craigService.selectcraigAttachments(craigNo);
 		model.addAttribute("craigImg", craigImg);
-		
+
 		log.debug("craig = {}", craig);
 		model.addAttribute("craig", craig);
 		model.addAttribute("chatroomId", chatroomId);
-		
+
 		return "chat/craigChatroom";
 	}
 }
--- src/main/webapp/WEB-INF/views/chat/craigChatList.jsp
+++ src/main/webapp/WEB-INF/views/chat/craigChatList.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,132 +22,93 @@
 	integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
 	crossorigin="anonymous"></script>
 <link rel="stylesheet"
-	href="${pageContext.request.contextPath }/resources/css/chat/chatroom.css" />
-<link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath }/resources/css/chat/craigChatList.css" />
 </head>
-<style>
-/* 주아체 */
-@font-face {
-    font-family: 'BMJUA';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-/* noto-sans */
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
 
-main#chatWrap {
-   max-width: 73rem;
-   mam-width: 73rem;
-   max-height: 50rem;
-   min-height: 50rem;
-   margin: 0 auto;
-   border: 1px solid rgba(0,0,0,.125);
-   font-family: 'Noto Sans KR', sans-serif;
- }
-#content {
-	border-bottom: 1px solid #eaebee;
-}
-
-/* 프로필바 */
-#prof-bar > a {
-	height: 72px;
-}
-
-/* 채팅목록  */
-#chatList > div#nickname {
-	height: 72px;
-}
-#chatList > div#nickname > span {
-	font-size: 20px;
-	font-weight: bold;
-}
-
-/* 스크롤바 관련  */
-.scrollarea {
-   overflow-y: auto;
-}
-.scrollarea::-webkit-scrollbar {
-    width: 8px; 
-}
-.scrollarea::-webkit-scrollbar-thumb {
-    background: rgba(178, 215, 181, 0.412); 
-    border-radius: 10px;
-    border: 1px solid #7FB77E;
-}
-.scrollarea::-webkit-scrollbar-track {
-    background: #F2F3F6; 
-}
-
-#chatLog {
-	width: 715px;
-}
-
-/* 채팅목록 */
-#chatList {
-
-}
-#chatList-wrap img{
-	width: 40px;
-	border-radius:50%;
-	float: left;
-}
-.chatList-header {
-	margin-right: 150px;
-}
-.col-10 {
-	max-width: 100%;	
-}
-</style>
-</head>
 <body>
-<div class="list-group list-group-flush border-bottom scrollarea">
-	<!-- 게시글정보 start -->
-	<div id="craig_bar">
-		<div class="craig_info_wrap">
-			<c:if test="${craig.attachments[0].reFilename == null}">
-				<img src="/oee/resources/images/OEE-LOGO2.png" alt="" />
-			</c:if>
-			<c:if test="${craig.attachments[0].reFilename != null}">
-				<img src="/oee/resources/upload/craig/${craig.attachments[0].reFilename}" alt="" />
-			</c:if>
-			<div class="craig_text">
-				<p class="craig_status">
-					<c:choose> 
-						<c:when test="${craig.state eq 'CR1'}">
-						예약중
-						</c:when>
-						<c:when test="${craig.state eq 'CR2'}">
-						판매중
-						</c:when>
-						<c:when test="${craig.state eq 'CR3'}">
-						판매완료
-						</c:when>
-					</c:choose>
-				</p> 
-				<p class="craig_name">${craig.title}</p>
-				<span class="price">
-					<fmt:formatNumber value="${craig.price}" pattern="#,###"/>원
-				</span>
+	<div class="chat">
+		<div class="card" style="min-height: 760px; max-height: 100%; min-width: 500px;">
+			<!-- 게시글정보 start -->
+			<div id="craig_bar">
+				<div class="craig_info_wrap">
+					<c:if test="${craigImg[0] == null}">	
+						<img style="width: 60px; height: 60px;" src="${pageContext.request.contextPath}/resources/images/OEE-LOGO2.png" alt="" />
+					</c:if>
+					<c:if test="${craigImg[0] != null}">
+						<img style="width: 60px; height: 60px;" src="${pageContext.request.contextPath}/resources/upload/craig/${craigImg[0].reFilename}"alt="" />
+					</c:if>
+					<div class="craig_text">
+						<p class="craig_status">
+							<c:choose>
+								<c:when test="${craig.state eq 'CR1'}">
+							예약중
+							</c:when>
+								<c:when test="${craig.state eq 'CR2'}">
+							판매중
+							</c:when>
+								<c:when test="${craig.state eq 'CR3'}">
+							판매완료
+							</c:when>
+							</c:choose>
+						</p>
+						<p class="craig_name">${craig.title}</p>
+						<span class="price"> <fmt:formatNumber
+								value="${craig.price}" pattern="#,###" />원
+						</span>
+					</div>
+				</div>
 			</div>
+			<!-- 게시글정보 end -->
+			
+			<!-- 채팅목록 start  -->
+			<div id="chatList" class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white">
+				<div class="list-group list-group-flush border-bottom scrollarea">
+					<c:forEach items="${chatModel}" var="chatMember">
+						<a id="toChatroom" href="#" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
+							<input type="hidden" name="chatroomId" value="${chatMember.lastChat.chatroomId}"/>
+							<div id="chatList-wrap">
+        	 					<img src="${pageContext.request.contextPath}/resources/upload/profile/${chatMember.otherProf}" alt="profileImg"/>
+        						<div class="chatList-header">
+        							<div id="chatUserInfo">
+		        		       			<strong class="mb-1">${chatMember.otherName}</strong>
+					          			<small>${chatMember.otherDong}</small>
+				        				<div class="last-chatting">${chatMember.lastChat.content}</div>
+        							</div>
+        						</div>
+        					</div>
+						</a>
+					</c:forEach>
+				</div>
+
+
+			</div>
+			<!-- 채팅목록 end  -->
 		</div>
-		<button type="button" class="btn btn-outline-secondary">약속잡기</button>
 	</div>
-	<!-- 게시글정보 end -->
+
+<script>	
+document.querySelector("#toChatroom").addEventListener('click', (e) => {
+const craigNo = ${craig.no};
+const memberId = `${craig.writer}`;
+const chatroomId = document.querySelector("#toChatroom input").value;
+
+const url = `${pageContext.request.contextPath}/chat/craigChat.do?chatroomId=\${chatroomId}&memberId=\${memberId}&craigNo=\${craigNo}`;
+const name = "craigChatroom";
+openPopup(url, name); 
+
+});
+	
 
 
-	<a href="#" class="list-group-item list-group-item-action py-3 lh-sm" aria-current="true">
-		<div id="chatList-wrap" class="d-flex w-100 align-items-center justify-content-between">
-			<img src="${pageContext.request.contextPath}/resources/images/oee.png" alt="임시이미지"/>
-        	<div class="chatList-header">
-	       		<strong class="mb-1">닉네임</strong>
-	          	<small>지역</small>
-			    <small>시간</small>
-		    	<div class="last-chatting small">마지막채팅내용</div> <!-- 앞에서부터 몇자까지 끊어서 출력해야할듯  -->
-        	</div> 
-        </div>
-      </a>
-</div>
+function openPopup(url, name){
+	let win;
+	win = window.open(url, name, 'scrollbars=yes,width=500,height=790,status=no,resizable=no');
+	win.opener.self;
+}
+
+
+</script>
 </body>
 </html>
