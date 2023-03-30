06c0bda4197d5bc348410be4018c72e2146d40ba
yeji0401 <splendideyee@gmail.com>
2023-03-30 15:01:36 +0900

    yeji_0330_commit

--- src/main/java/com/sh/oee/admin/controller/AdminController.java
+++ src/main/java/com/sh/oee/admin/controller/AdminController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sh.oee.admin.model.service.AdminService;
+import com.sh.oee.craig.model.dto.Craig;
+import com.sh.oee.local.model.dto.Local;
 import com.sh.oee.member.model.dto.Member;
 import com.sh.oee.report.model.dto.BoardReport;
 import com.sh.oee.report.model.dto.UserReport;
+import com.sh.oee.together.model.dto.Together;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,11 +26,36 @@ public class AdminController {
 	@Autowired
 	public AdminService adminService;
 	
+	/* select 조회 
+	 * - 페이징 처리 해야 함 
+	 */
+	
 	@GetMapping("/adminMemberList.do")
 	public void adminMemberList(Model model) {
 		List<Member> adminMemberList = adminService.selectAdminMemberList();
 		log.debug("adminMemberList = ()", adminMemberList);
 		model.addAttribute("adminMemberList", adminMemberList);
+	}
+	
+	@GetMapping("/adminCraigList.do")
+	public void adminCraigList(Model model) {
+		List<Craig> adminCraigList = adminService.selectAdminCraigList();
+		log.debug("adminCraigList = ()", adminCraigList);
+		model.addAttribute("adminCraigList", adminCraigList);
+	}
+	
+	@GetMapping("/adminLocalList.do")
+	public void adminLocalList(Model model) {
+		List<Local> adminLocalList = adminService.selectAdminLocalList();
+		log.debug("adminLocalList = ()", adminLocalList);
+		model.addAttribute("adminUserReport", adminLocalList);
+	}
+	
+	@GetMapping("/adminTogetherList.do")
+	public void adminTogetherList(Model model) {
+		List<Together> adminTogetherList = adminService.selectAdminTogetherList();
+		log.debug("adminTogetherList = ()", adminTogetherList);
+		model.addAttribute("adminTogetherList", adminTogetherList);
 	}
 	
 	@GetMapping("/adminBoardReport.do")
@@ -43,4 +71,5 @@ public class AdminController {
 		log.debug("adminUserReport = ()", adminUserReport);
 		model.addAttribute("adminUserReport", adminUserReport);
 	}
+	
 }
--- src/main/java/com/sh/oee/admin/model/service/AdminService.java
+++ src/main/java/com/sh/oee/admin/model/service/AdminService.java
@@ -2,13 +2,22 @@ package com.sh.oee.admin.model.service;
 
 import java.util.List;
 
+import com.sh.oee.craig.model.dto.Craig;
+import com.sh.oee.local.model.dto.Local;
 import com.sh.oee.member.model.dto.Member;
 import com.sh.oee.report.model.dto.BoardReport;
 import com.sh.oee.report.model.dto.UserReport;
+import com.sh.oee.together.model.dto.Together;
 
 public interface AdminService {
 
 	List<Member> selectAdminMemberList();
+	
+	List<Craig> selectAdminCraigList();
+
+	List<Local> selectAdminLocalList();
+
+	List<Together> selectAdminTogetherList();
 
 	List<BoardReport> selectAdminBoardReport();
 
